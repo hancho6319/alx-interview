34a1f040c1c4882dab9dcfa19258d9ad628d5843
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-06-16 14:33:38 +0100

    GitHub repository: alx-interview     Directory: 0x02-minimum_operations     File: 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,9 +2,11 @@ def minOperations(n):
     """
     0-minoperations.py file for testing
     """
-    if n <= 1: return 0
+    if n <= 1:
+        return 0
 
-    operations, factor = 0, 2
+    operations = 0
+    factor = 2
 
     while n > 1:
         if n % factor == 0:
