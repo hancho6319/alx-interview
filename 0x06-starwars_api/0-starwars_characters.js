98462603583880fc7e5a808a2c50e824ddb49646
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-07-14 18:30:53 +0100

    GitHub repository: alx-frontend-for-fun     Directory: form     File: 01-article.html, 01-styles.css

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,25 @@
 #!/usr/bin/node
 
-
 const axios = require('axios');
 
 const movieId = process.argv[2];
 
 if (!movieId) {
-    console.log('Please provide a movie ID as the first argument.');
-    process.exit(1);
+  console.log('Please provide a movie ID as the first argument.');
+  process.exit(1);
 }
 
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 axios.get(apiUrl)
-    .then(response => {
-        const characters = response.data.characters;
-        const characterPromises = characters.map(url => axios.get(url).then(res => res.data.name));
-        return Promise.all(characterPromises);
-    })
-    .then(characterNames => {
-        characterNames.forEach(name => console.log(name));
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error.message);
-    });
-
+  .then(response => {
+    const characters = response.data.characters;
+    const characterPromises = characters.map(url => axios.get(url).then(res => res.data.name));
+    return Promise.all(characterPromises);
+  })
+  .then(characterNames => {
+    characterNames.forEach(name => console.log(name));
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error.message);
+  });
