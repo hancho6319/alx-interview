1b657a94fe6f13afa7c122b51a9adce33220ccac
Haruna Auwalu <hancho6319@hancho6319.hancho6319>
2024-07-21 21:48:04 +0100

    GitHub repository: alx-frontend-for-fun     Directory: form     File: 01-article.html, 01-styles.css

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+def rotate_2d_matrix(matrix):
+    """
+    This is Rotates an n x n 2D matrix 90 degrees clockwise.
+    """
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(n):
+        matrix[i].reverse()
+
