c56af3996a3d7d554a6ceeca06e1c1a20842db8a
hancho6319 <abbaauwalharuna@gmail.com>
2024-12-01 01:24:30 +0100

    MAKING CHANGE

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ 0x08. Making Change """
 
+
 def makeChange(coins, total):
     """ Return: fewest number of coins needed to meet total """
     if total <= 0:
         return 0
-    
+
     """ To sort the coins in reverse """
     coins.sort(reverse=True)
     number_of_changes = 0
@@ -18,7 +19,7 @@ def makeChange(coins, total):
 
         if total_changes == total:
             return total_changes
-        
+
         total_changes -= coin
         number_of_changes -= 1
 
