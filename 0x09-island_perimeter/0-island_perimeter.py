3bf477334b28b8aec34fb8642d5e86f7b32e685b
hancho6319 <abbaauwalharuna@gmail.com>
2024-12-06 03:29:56 +0100

    [200~GitHub repository: alx-interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,29 @@
 def island_perimeter(grid):
+    """
+     0x09. Island Perimeter
+    """
     perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0])
     
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(grid[0]):
             if grid[i][j] == 1:
                 perimeter += 4
                 
                 # Check above
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
                 
                 # Check left
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                    perimeter -= 1
+
+                # Check below
+                if i < len(grid) - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                
+                # Check right
+                if j < len(grid(0)) - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
     
     return perimeter
 
