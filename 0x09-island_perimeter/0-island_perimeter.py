82504722151e0a1f5ed41259f622fc919c5dfbf6
hancho6319 <abbaauwalharuna@gmail.com>
2024-12-06 04:01:25 +0100

    0-island_perimeter.py:9:26: W291 trailing whitespace 0-island_perimeter.py:12:1: W293 blank line contains whitespace 0-island_perimeter.py:35:1: W391 blank line at end of file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,7 +11,7 @@ def island_perimeter(grid):
     perimeter = 0
 
     for i in range(len(grid)):
-        for j in range(grid[0]):
+        for j in range(len(grid[0])):
             if grid[i][j] == 1:
                 perimeter += 4
 
