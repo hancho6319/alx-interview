17b755b2550144be427f4dced28159d4da61581a
hancho6319 <abbaauwalharuna@gmail.com>
2024-12-06 03:45:31 +0100

    0-island_perimeter.py:9:26: W291 trailing whitespace 0-island_perimeter.py:12:1: W293 blank line contains whitespace 0-island_perimeter.py:35:1: W391 blank line at end of file

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
+"0x09. Island Perimeter"
+
 
 def island_perimeter(grid):
     """
-     0x09. Island Perimeter
+        Create a function def island_perimeter(grid)
+        that returns the perimeter of the island
+        described in grid
     """
     perimeter = 0
-    
+
     for i in range(len(grid)):
         for j in range(grid[0]):
             if grid[i][j] == 1:
                 perimeter += 4
-                
+
                 # Check above
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
-                
+
                 # Check left
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 1
@@ -22,10 +26,9 @@ def island_perimeter(grid):
                 # Check below
                 if i < len(grid) - 1 and grid[i + 1][j] == 1:
                     perimeter -= 1
-                
+
                 # Check right
                 if j < len(grid(0)) - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
-    
-    return perimeter
 
+    return perimeter
